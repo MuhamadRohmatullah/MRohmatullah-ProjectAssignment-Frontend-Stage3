40cdafb79ca3139be76ea393175d0202a4f42c0f
MuhamadRohmatullah <m.rohmatullah10@gmail.com>
2024-02-20 14:48:03 +0700

    memperbaiki erorr di mix saat menjalankan npm run watch

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,4 +3,4 @@ const mix = require("laravel-mix");
 mix.js('resources/js/app.js', 'public/js/app.js')
    .vue()
    .postCss('resources/css/app.css', 'public/css/app.css', [])
-   .sass('resorces/sass/app.scss', 'public/css/app.css');
+   .sass('resources/sass/app.scss', 'public/css/app.css');
