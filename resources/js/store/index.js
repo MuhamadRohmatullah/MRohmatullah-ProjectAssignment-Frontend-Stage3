437130edd056510c631e68f550210dab8915aee6
MuhamadRohmatullah <m.rohmatullah10@gmail.com>
2024-02-29 12:33:10 +0700

    konfigurasi vuex dan mengimport axios didalamnnya, menambahkan state, getters, actios dan mutation

---
+++ resources/js/store/index.js
@@ -0,0 +1,37 @@
+import axios from "axios";
+import { createStore } from "vuex";
+
+export default createStore({
+
+    state:{
+        product:{},
+        chart : []
+    },
+    getters:{
+        getProduct(state){
+            return state.product;
+        },
+        getChart(state){
+            return state.chart;
+        }
+    },
+    actions:{
+        getDataProduct : async (context) =>{
+            let response = await axios.get('api/getAllProduct');
+            
+            context.commit('ADD_PRODUCT', response.data);
+        },
+        addToChart(context, payload){
+            context.commit('ADD_CHART', payload);
+        }
+    },
+    mutations:{
+        ADD_PRODUCT(state, payload){
+            state.product = payload.data;
+        },
+        ADD_CHART(state, payload){
+            state.chart.push(state.product[payload]);
+        }
+    }
+
+})
