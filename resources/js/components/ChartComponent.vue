f984cc7c0bb5f2cb3ccffd7c6c3120e97d3b361f
MuhamadRohmatullah <m.rohmatullah10@gmail.com>
2024-03-03 00:47:29 +0700

    menambahkan ikon keranjang, menambahkan header unutk keranjang, memperbaiki style dan tata letak keranjang, menambahkan getters unutk mengambil data total harga dan barang yang ada di cart di state, menambahkan dispatch di created dan menambahkan styling unutk halaman cart

--- resources/js/components/ChartComponent.vue
+++ resources/js/components/ChartComponent.vue
@@ -1,38 +1,111 @@
 <template>
-    <div class="card">
+ <div class="container-fluid">
+    <div class="row justify-content-center">
+        <div class="col text-center fw-bold">
+             <font-awesome-icon id="cart" icon="fa-solid fa-cart-shopping" size="2xl"/><span class="fs-2">Keranjang</span>
+        </div>
+    </div>
+    <font-awesome-icon id="back" icon="fa-solid fa-arrow-left-long" size="xl" title="go back" @click="goBack()"/>
+   
+    <div class="card p-auto shadow-sm">
         <div class="card-body">
-            <table class="table">
+            <table class="table table-hover table-info">
                 <thead>
                     <tr>
                         <th>Nama</th>
                         <th>Jumlah</th>
                         <th>harga</th>
+                        <th>Action</th>
                     </tr>
                 </thead>
-                <tbody>
-                    <tr v-for="(item, index) in chartItem" :key="index">
+                <tbody v-for="(item, index) in chartItem" :key="index">
+                    <tr v-if="item.qty != 0">
                         <td>{{item.name}}</td>
-                        <td></td>
-                        <td>{{item.price}}</td>
+                        <td>{{item.qty}}</td>
+                        <td>{{item.price*item.qty}}</td>
+                        <td><font-awesome-icon id="delete" icon="fa-solid fa-trash" size="xl" style="color: #d01616;"  @click="deleteCart(index)"/></td>
                     </tr>
                 </tbody>
             </table>
         </div>
     </div>
-    <button class="btn btn-primary" @click="goBack()">Back</button>
+    
+    <div class="fixed-bottom">
+        <div class="card shadow-sm" id="footer">
+            <div class="card-body">
+                <div class="row">
+                    <div class="col">
+                        <h4>Total <span v-if="total != 0">: {{total}}</span></h4>
+                    </div>
+                    <div class="col text-end">
+                        
+                        <button class="btn btn-warning" @click="goToPayment()"><font-awesome-icon icon="fa-regular fa-square-check" size="xl" style="color: #0a0a0a;" /><span class="fw-bold"> Checkout</span></button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+    </div>
+</div>
+    
 </template>
 <script>
 import {mapGetters} from "vuex";
 export default {
+    data(){
+        return{
+        }
+    },
     methods:{
         goBack(){
             this.$router.go(-1);
+        },
+        deleteCart(index){
+            this.$store.dispatch('deleteCart', index);
+            this.$store.dispatch('hitungTotal');
+        },
+        goToPayment(){
+           this.$router.push({name : "payment"});
         }
+
     },
     computed:{
         ...mapGetters({
-            chartItem : "getChart"
+            chartItem : "getChart",
+            total : "getTotal"
         })
+    },
+    created(){
+        this.$store.dispatch('hitungTotal');
     }
 }
 </script>
+<style scoped>
+
+*{
+    font-family: "Fira Sans Extra Condensed", sans-serif;
+    box-sizing: border-box;
+}
+
+
+#delete:hover{
+    cursor: pointer;
+    width: 18px;
+}
+
+.card{
+    border-radius: 0;
+}
+
+#back{
+    width: 30px;
+    cursor: pointer;
+}
+
+#back:hover{
+    color: rgb(133, 133, 130);
+}
+
+</style>
