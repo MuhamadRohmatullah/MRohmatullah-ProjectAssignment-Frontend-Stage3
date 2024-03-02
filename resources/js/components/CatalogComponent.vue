139af6f5d670ebc893cfed902b347423c528c6c2
MuhamadRohmatullah <m.rohmatullah10@gmail.com>
2024-03-03 00:44:08 +0700

    menambhakan container fluid, menmabahkan component header top, memperbaiki jarak tata letak, menambahkan icon fontawesome, mengambil data chart dengan getters di state dan menambhakan style unutk halaman catalog

--- resources/js/components/CatalogComponent.vue
+++ resources/js/components/CatalogComponent.vue
@@ -1,23 +1,26 @@
 <template>
-    <div class="container">
+    <div class="container-fluid">
         <div class="row">
             <div class="col">
-                <button class="btn btn-success" @click="getToChart()">Chart</button>
+                <top/>
+                
             </div>
         </div>
 
-        <div class="row">
-            <div v-for="(list, index) in foodList" :key="index" class="col">
-                <div class="card text-center">
+        <div class="row mt-5">
+            <div v-for="(list, index) in foodList" :key="index" class="col-sm-6 col-md-4 col-lg-3">
+                <div class="card text-center mt-5 shadow-sm">
                     <div class="card-body">
-                            <h5>{{list.name}}</h5>
+                            <h5 class="fw-bold">{{list.name}}</h5>
                             <h6>{{list.description}}</h6>
                     </div>
                     <div class="card-footer">
                         <h6>Stok : {{list.stok}}</h6>
                         <h6 :class="list.stok == 0 ? 'sold' : ''">Rp.{{list.price}}</h6>
-                        <button v-if="list.stok != 0" class="btn btn-primary" @click="addChart(index)">Add To Chart</button>
-                        <button v-else class="btn btn-danger">SoldOut</button>
+                        <button v-if="list.stok != 0" @click="addChart(index)" class="btn btn-success" id="chrt">
+                            <font-awesome-icon  icon="fa-solid fa-cart-plus" size="xl" id="addcart"/>
+                        </button>
+                        <buton v-else class="btn btn-danger" text="Sold Out"/>
                     </div>
                 </div>
             </div>
@@ -34,28 +37,55 @@ export default {
         }
     },
     methods:{
-        getToChart(){
-            this.$router.push({name : "chart"});
-        },
         addChart(index){
             this.$store.dispatch("addToChart", index);
         }
     },
     computed:{
         ...mapGetters({
-            foodList : 'getProduct'
-        })
+            foodList : 'getProduct',
+            tochart : 'getTocart'
+        }),
     },
     created(){
-         this.$store.dispatch('getDataProduct');
+        this.$store.dispatch('getDataProduct'); 
     }
 }
 </script>
-<style>
+<style scoped>
 
 .sold{
     text-decoration: line-through;
     color: rgb(97, 97, 97) !important;
+}
+
+.card{
+    border-radius: 0;
+    width: 100%;
+    height: 220px;
+}
+
+#addcart{
+    color: #ffffff;
+    cursor: pointer;
+}
+
+.btn{
+    border-radius: 0;
+}
+
+#chrt{
+    background-color: rgb(27, 245, 125);
+    border: 0;
+}
+
+#chrt:hover{
+    background-color: rgba(41, 245, 133, 0.596);
+}
+
+*{
+    font-family: "Fira Sans Extra Condensed", sans-serif;
+    box-sizing: border-box;
 }
 
 </style>
