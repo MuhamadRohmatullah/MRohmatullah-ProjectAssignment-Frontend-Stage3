1019e886765bfbf1a9a8fb4058660763e6dc3e93
MuhamadRohmatullah <m.rohmatullah10@gmail.com>
2024-03-03 17:30:05 +0700

    menambah icon font awesomee dan mneghapus footer yang dipindahkan ke footer chart component

--- resources/js/components/ChartComponent.vue
+++ resources/js/components/ChartComponent.vue
@@ -1,12 +1,14 @@
 <template>
  <div class="container-fluid">
-    <div class="row justify-content-center">
-        <div class="col text-center fw-bold">
-             <font-awesome-icon id="cart" icon="fa-solid fa-cart-shopping" size="2xl"/><span class="fs-2">Keranjang</span>
+    <div class="row">
+        <div class="col-lg-5 col-md-4 col-sm-4 col-4">
+            <font-awesome-icon id="back" icon="fa-solid fa-arrow-left-long" size="2xl" title="go back" @click="goBack()"/>
+        </div>
+        <div class="col-lg-7 col-md-8 col-sm-8 col-8 text-start fw-bold">
+            <font-awesome-icon id="cart" icon="fa-solid fa-cart-shopping" size="2xl"/><span class="fs-2">Keranjang</span>
         </div>
     </div>
-    <font-awesome-icon id="back" icon="fa-solid fa-arrow-left-long" size="xl" title="go back" @click="goBack()"/>
-   
+    
     <div class="card p-auto shadow-sm">
         <div class="card-body">
             <table class="table table-hover table-info">
@@ -21,7 +23,7 @@
                 <tbody v-for="(item, index) in chartItem" :key="index">
                     <tr v-if="item.qty != 0">
                         <td>{{item.name}}</td>
-                        <td>{{item.qty}}</td>
+                        <td  class="ms-2">{{item.qty}}</td>
                         <td>{{item.price*item.qty}}</td>
                         <td><font-awesome-icon id="delete" icon="fa-solid fa-trash" size="xl" style="color: #d01616;"  @click="deleteCart(index)"/></td>
                     </tr>
@@ -29,25 +31,9 @@
             </table>
         </div>
     </div>
-    
-    <div class="fixed-bottom">
-        <div class="card shadow-sm" id="footer">
-            <div class="card-body">
-                <div class="row">
-                    <div class="col">
-                        <h4>Total <span v-if="total != 0">: {{total}}</span></h4>
-                    </div>
-                    <div class="col text-end">
-                        
-                        <button class="btn btn-warning" @click="goToPayment()"><font-awesome-icon icon="fa-regular fa-square-check" size="xl" style="color: #0a0a0a;" /><span class="fw-bold"> Checkout</span></button>
-                    </div>
-                </div>
-            </div>
-        </div>
 
+    <bot/>
 
-
-    </div>
 </div>
     
 </template>
@@ -65,47 +51,28 @@ export default {
         deleteCart(index){
             this.$store.dispatch('deleteCart', index);
             this.$store.dispatch('hitungTotal');
-        },
-        goToPayment(){
-           this.$router.push({name : "payment"});
         }
-
     },
     computed:{
         ...mapGetters({
             chartItem : "getChart",
-            total : "getTotal"
         })
     },
     created(){
         this.$store.dispatch('hitungTotal');
     }
 }
+
 </script>
 <style scoped>
-
-*{
-    font-family: "Fira Sans Extra Condensed", sans-serif;
-    box-sizing: border-box;
-}
-
 
 #delete:hover{
     cursor: pointer;
     width: 18px;
 }
 
-.card{
-    border-radius: 0;
-}
-
-#back{
-    width: 30px;
-    cursor: pointer;
-}
-
-#back:hover{
-    color: rgb(133, 133, 130);
+.total{
+    color : rgb(42, 153, 245);
 }
 
 </style>
