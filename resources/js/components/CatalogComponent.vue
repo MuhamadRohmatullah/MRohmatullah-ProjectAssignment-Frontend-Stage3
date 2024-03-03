d3fd976ed7ef92fe3dc515a559a2e0243f5cb0ad
MuhamadRohmatullah <m.rohmatullah10@gmail.com>
2024-03-03 17:28:03 +0700

    menghapus header yang dipindahkan ke component main page, merapikan kode

--- resources/js/components/CatalogComponent.vue
+++ resources/js/components/CatalogComponent.vue
@@ -1,18 +1,12 @@
 <template>
     <div class="container-fluid">
-        <div class="row">
-            <div class="col">
-                <top/>
-                
-            </div>
-        </div>
 
-        <div class="row mt-5">
+        <div class="row">
             <div v-for="(list, index) in foodList" :key="index" class="col-sm-6 col-md-4 col-lg-3">
-                <div class="card text-center mt-5 shadow-sm">
+                <div class="card text-center mt-2 shadow-sm">
                     <div class="card-body">
-                            <h5 class="fw-bold">{{list.name}}</h5>
-                            <h6>{{list.description}}</h6>
+                        <h5 class="fw-bold">{{list.name}}</h5>
+                        <h6>{{list.description}}</h6>
                     </div>
                     <div class="card-footer">
                         <h6>Stok : {{list.stok}}</h6>
@@ -25,6 +19,7 @@
                 </div>
             </div>
         </div>
+
     </div>
 </template>
 <script>
@@ -47,7 +42,7 @@ export default {
             tochart : 'getTocart'
         }),
     },
-    created(){
+    beforeCreate(){
         this.$store.dispatch('getDataProduct'); 
     }
 }
@@ -60,7 +55,6 @@ export default {
 }
 
 .card{
-    border-radius: 0;
     width: 100%;
     height: 220px;
 }
@@ -81,11 +75,6 @@ export default {
 
 #chrt:hover{
     background-color: rgba(41, 245, 133, 0.596);
-}
-
-*{
-    font-family: "Fira Sans Extra Condensed", sans-serif;
-    box-sizing: border-box;
 }
 
 </style>
