145aa37f61db7761023475aebaef0d1e11d8f724
MuhamadRohmatullah <m.rohmatullah10@gmail.com>
2024-03-03 17:34:46 +0700

    membuat button component, menambahkan proops supaya bisa digunakan di banyak component dan ada masukan teks unutk ditampilkan

---
+++ resources/js/components/ButtonComponent.vue
@@ -0,0 +1,24 @@
+<template>
+    <button class="btn" @click="funClick()">{{text}}</button>
+</template>
+<script>
+export default {
+    emits:['emits-click'],
+    props:{
+        text:{
+            type: String,
+            default: ""
+        }
+    },
+    methods:{
+        funClick(){
+            this.$emit('emits-click');
+        }
+    }
+}
+</script>
+<style scoped>
+.btn{
+    border-radius: 0;
+}
+</style>
